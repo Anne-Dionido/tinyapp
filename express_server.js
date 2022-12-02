5fb5c0d0f897a1fb03826100f7e88bc9f77642bc
Anne-Dionido <anne.dionido@telus.com>
2022-12-01 22:46:34 -0700

    commit all

--- express_server.js
+++ express_server.js
@@ -34,3 +34,8 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
